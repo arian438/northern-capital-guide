931251825c75baac88356e7134f765fadc1a13cb
arian438 <kae74ru@gmail.com>
2026-06-23 16:57:51 +0500

    fix

--- api/index.js
+++ api/index.js
@@ -1,8 +1,1 @@
-const path = require('path');
-
-// Подключаем зависимости бэкенда при запуске serverless-функции Vercel
-module.paths.unshift(path.join(__dirname, '../backend/node_modules'));
-
-require('dotenv').config({ path: path.join(__dirname, '../backend/.env') });
-
 module.exports = require('../backend/src/app');
